c8e757478255258d6ed925cdc7e5f8cf99bec844
vishwajeethaldar <onlyjeet3@gmail.com>
2022-11-09 17:35:27 +0530

    updated css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import {Box, Input, Button} from '@chakra-ui/react'
 import { MouseEvent } from 'react'
 export default function Home() {
 
   
   return (
-    <div className={styles.container}>
+    <div >
       <Head>
         <title> ZARA</title>
         <meta name="description" content=" Purchase " />
