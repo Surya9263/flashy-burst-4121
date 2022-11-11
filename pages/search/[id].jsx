50ea505e2dd68a88292acf945c45dc211446384c
Surya Srivastava <suryasrivastava724@gmail.com>
2022-11-11 21:46:59 +0530

    single product

--- pages/search/[id].jsx
+++ pages/search/[id].jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import {Box, Flex, HStack, Link, Stack, Text, VStack} from "@chakra-ui/react"
+import React, { useState } from 'react'
+import {Box, Flex, HStack, Image, Link, Stack, Text, VStack} from "@chakra-ui/react"
 const SingleProduct = () => {
+  const [isShowSupImg,setIsShowSupImg]=useState(false)
+  const [mainImg,setMainImg]=useState("https://static.zara.net/photos///2022/I/0/1/p/8263/646/611/2/w/750/8263646611_1_1_1.jpg?ts=1663157747603")
   return (
     <HStack justifyContent={"center"} spacing={"25px"} fontFamily={"Neue-Helvetica"}>
         <Box w={"15%"}>
-            <Stack>
+            <Stack border={"1px solid red"}>
                 <Box w={"70%"}><Text lineHeight={"18px"} fontSize={"13px"} as={"b"}>MATERIALS, CARE AND ORIGIN</Text></Box>
                 <Box><Text lineHeight={"16px"} fontSize={"11px"} as={"b"}>MATERIALS</Text></Box>
                 <Box><Text lineHeight={"16px"} fontSize={"13px"}>We work with monitoring programmes to ensure compliance with safety, health and quality standards for our products. </Text></Box>
@@ -12,7 +14,18 @@ const SingleProduct = () => {
                 <Box><Link><Text textDecoration={"underline"} lineHeight={"16px"} fontSize={"12px"}>View more</Text></Link></Box>
             </Stack>
         </Box>
-        <Box w={"30%"} border={"1px solid red"}>Image</Box>
+        <Box w={"30%"}>
+          <Flex onMouseOver={()=>{setIsShowSupImg(true)}} onMouseLeave={()=>setIsShowSupImg(false)} gap={4} border={"1px solid green"}>
+          <Link><Image transition={'ease-in-out'} height={"xl"} src={mainImg}/></Link>
+          <Stack hidden={!isShowSupImg} spacing={'6'}>
+          <Box w='40px' h='40px'><Image cursor={"pointer"} onClick={(e)=>setMainImg(e.target.src)} src='https://static.zara.net/photos///2022/I/0/1/p/8263/646/611/2/w/750/8263646611_1_1_1.jpg?ts=1663157747603'/></Box>
+          <Box w='40px' h='40px'><Image cursor={"pointer"} onClick={(e)=>setMainImg(e.target.src)} src='https://static.zara.net/photos///2022/I/0/1/p/8263/646/611/2/w/750/8263646611_2_1_1.jpg?ts=1663157750866'/></Box>
+          <Box w='40px' h='40px'><Image cursor={"pointer"} onClick={(e)=>setMainImg(e.target.src)} src='https://static.zara.net/photos///2022/I/0/1/p/8263/646/611/2/w/750/8263646611_2_2_1.jpg?ts=1663157763818'/></Box>
+          <Box w='40px' h='40px'><Image cursor={"pointer"} onClick={(e)=>setMainImg(e.target.src)} src='https://static.zara.net/photos///2022/I/0/1/p/8263/646/611/2/w/750/8263646611_1_1_1.jpg?ts=1663157747603'/></Box>
+          <Box w='40px' h='40px'><Image cursor={"pointer"} onClick={(e)=>setMainImg(e.target.src)} src='https://static.zara.net/photos///2022/I/0/1/p/8263/646/611/2/w/750/8263646611_1_1_1.jpg?ts=1663157747603'/></Box>
+        </Stack>
+        </Flex>
+        </Box>
         <Box w={"30%"} border={"1px solid red"}>Size and all</Box>
     </HStack>
   )
